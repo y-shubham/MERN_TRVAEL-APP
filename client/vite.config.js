5242e437804a972814ebda224a32b735d6e1854b
Shubham Yadav <chillitech111@gmail.com>
2024-10-13 17:13:09 +0530

    seventh commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://mern-trvael-app.onrender.com",
+        target: "http://localhost:8000",
         secure: false,
       },
     },
