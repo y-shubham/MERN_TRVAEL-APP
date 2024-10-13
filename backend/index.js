5c01b9aeb03bf59a22c838bc2ffc827b051bb083
Shubham Yadav <chillitech111@gmail.com>
2024-10-13 14:48:30 +0530

    third commit

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,12 @@ import ratingRoute from "./routes/rating.route.js";
 import bookingRoute from "./routes/booking.route.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import path from "path";
 import cors from "cors";
+
 const app = express();
 dotenv.config();
 
-const __dirname = path.resolve();
-
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -21,35 +20,25 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+// Middleware
 app.use(
   cors({
-    origin: process.env.SERVER_URL,
+    origin: process.env.SERVER_URL, // Adjust this to your frontend URL
   })
 );
 app.use(express.json());
 app.use(cookieParser());
 
+// API routes
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/package", packageRoute);
 app.use("/api/rating", ratingRoute);
 app.use("/api/booking", bookingRoute);
 
-if (process.env.NODE_ENV_CUSTOM === "production") {
-  //static files
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+// Start server on Vercel's PORT or fallback to 8000
+const PORT = process.env.PORT || 8000;
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-  });
-} else {
-  // //rest api
-  app.use("/", (req, res) => {
-    res.send("Welcome to travel and tourism app");
-  });
-}
-
-//port
-app.listen(8000, () => {
-  console.log("listening on 8000");
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
